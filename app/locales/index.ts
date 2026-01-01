8a614babd78efa1ed2acc341e9239c77bb8c0077
afiseleo <aaaa50306@gmail.com>
2026-01-02 00:06:03 +0800

    Add Traditional Chinese localization for the application

--- app/locales/index.ts
+++ app/locales/index.ts
@@ -4,7 +4,7 @@ import type { Resource } from "i18next";
 import en from "./en/index";
 import ko from "./ko/index";
 import ja from "./ja/index";
-import zhHantTW from "./zh-Hant-TW/index";
-const resources = { en, ko, ja, "zh-Hant-TW": zhHantTW } satisfies Resource;
+import zh_Hant from "./zh_Hant/index";
+const resources = { en, ko, ja, zh_Hant } satisfies Resource;
 
 export default resources;
